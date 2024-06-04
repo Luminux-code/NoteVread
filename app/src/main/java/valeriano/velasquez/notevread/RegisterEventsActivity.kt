1d1f6bfe0658028a48e35c586c8967b2b40ec005
joseph <jovave25@gmail.com>
2024-06-04 10:15:34 -0500

    Interfaz basica

--- app/src/main/java/valeriano/velasquez/notevread/RegisterEventsActivity.kt
+++ app/src/main/java/valeriano/velasquez/notevread/RegisterEventsActivity.kt
@@ -1,20 +1,16 @@
 package valeriano.velasquez.notevread
 
-import valeriano.velasquez.notevread.R
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class MainActivity : AppCompatActivity() {
+class RegisterEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_events)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -22,12 +18,5 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
-        val button = findViewById<Button>(R.id.buttonWelcome);
-
-        button.setOnClickListener {
-            val intent = Intent(this@MainActivity, FeaturesActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
